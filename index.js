4739fcaddd352fad8512163b8d91e5578eb55e3f
SxL <tulip_sxl@outlook.com>
2019-02-01 14:23:58 +0800

    [REFACTOR] reorganize files to index.js

--- index.js
+++ index.js
@@ -2,19 +2,56 @@ const path = require('path');
 const Koa = require('koa');
 const bodyparser = require('koa-bodyparser');
 const serve = require('koa-static');
+const Sequelize = require('sequelize');
+const Router = require('koa-router');
 
 global.config = require(path.resolve('config.json'));
 
-const router = require('./src/router');
-const app = new Koa();
+const sequelize = new Sequelize(config.database.name, config.database.username, config.database.password, {
+  dialect: 'mysql',
+  host: config.database.host,
+});
 
-app.use(bodyparser({
-  enableTypes: ['json', 'form', 'text'],
-  extendTypes: {
-    text: ['text/plain'] 
+const Captcha = module.exports = sequelize.define('training', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  value: {
+    type: Sequelize.STRING(4),
+    allowNull: false
+  },
+  captcha: {
+    type: Sequelize.BLOB,
+    allowNull: false,
+    get() {
+      return this.getDataValue('captcha').toString('base64');
+    }
   }
-}));
+}, {
+  timestamps: false,
+  freezeTableName: true,
+});
+
+const router = new Router({
+  prefix: '/captcha'
+});
+
+router.get('/:captchaId', async ctx => {
+  ctx.body = await Captcha.findByPk(ctx.params.captchaId)
+}).patch('/:captchaId', async ctx => {
+  const captcha = await Captcha.findByPk(ctx.params.captchaId)
+  captcha.value = ctx.request.body.value;
+  await captcha.save({
+    fields: ['value']
+  })
+
+  ctx.body = captcha;
+})
+
+const app = new Koa();
+app.use(bodyparser());
 app.use(router.routes());
 app.use(serve(path.resolve(config.static.path)));
-
 app.listen(config.http.port, config.http.host);
--- src/router/index.js
+++
@@ -1,6 +0,0 @@
-const Router = require('koa-router');
-const captchaRouter = require('./captcha');
-
-const router = module.exports = new Router();
-
-router.use(captchaRouter.routes())
